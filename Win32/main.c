e9f1b9059f8d9cd50120644754c1ac0e1cb9c27c
bcrew1375 <no-reply>
2018-09-27 03:59:32 -0400

    Initial commit

---
+++ Win32/main.c
@@ -0,0 +1,164 @@
+#include <windows.h>
+#include <malloc.h>
+#include "resource.h"
+
+extern LoadRomFile(char filename[]);
+extern InitializeSDL();
+extern CloseSDL();
+extern OpenSDLWindow();
+extern Reset();
+extern Interpret();
+extern CheckSDLEvents();
+
+unsigned char *romBuffer;
+
+WNDCLASSEX wc;
+HWND hwnd;
+MSG Msg;
+
+LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
+{
+	switch(msg)
+	{
+	case WM_CREATE:
+		{
+			if (!(romBuffer = malloc(0x1050)))
+			{
+				MessageBox(hwnd, "Could not allocate memory for file buffer!", "Error!", MB_OK);
+				DestroyWindow(hwnd);
+			}
+			if (InitializeSDL() == -1)
+			{
+				MessageBox(hwnd, "Could not initialize SDL!", "Error!", MB_OK);
+				DestroyWindow(hwnd);
+			}
+		}
+	break;
+	case WM_CLOSE:
+		{
+			CloseSDL();
+			DestroyWindow(hwnd);
+			exit(0);
+		}
+	break;
+	case WM_DESTROY:
+		{
+			CloseSDL();
+			PostQuitMessage(0);
+		}
+	break;
+	case WM_COMMAND:
+		switch (LOWORD(wParam))
+		{
+		case MENUID_FILE_OPEN:
+			{
+				OPENFILENAME ofn;
+				char szFileName[MAX_PATH] = "";
+
+				ZeroMemory(&ofn, sizeof(ofn));
+
+				ofn.lStructSize = sizeof(ofn);
+				ofn.hwndOwner = hwnd;
+				ofn.lpstrFilter = "All Files (*.*)\0*.*\0";
+				ofn.lpstrFile = szFileName;
+				ofn.nMaxFile = MAX_PATH;
+				ofn.Flags = OFN_EXPLORER | OFN_FILEMUSTEXIST | OFN_HIDEREADONLY;
+				ofn.lpstrDefExt = "";
+				
+				if(GetOpenFileName(&ofn))
+				{
+					switch(LoadRomFile(szFileName))
+					{
+					case -1:
+						{
+							MessageBox(hwnd, "Could not open file in Read-Only Binary mode!", "Error!", MB_OK);
+						}
+					break;
+					case -2:
+						{
+							MessageBox(hwnd, "Could not read from file!", "Error!", MB_OK);
+						}
+					break;
+					}
+				}
+				Reset();
+			}
+		break;
+		case MENUID_FILE_EXIT:
+			{
+				CloseSDL();
+				DestroyWindow(hwnd);
+				exit(0);
+			}
+		break;
+		case MENUID_INTERPRETER_RUN:
+			{
+				if (OpenSDLWindow() == -1)
+					MessageBox(hwnd, "Could not open SDL window", "Error!", MB_OK);
+
+				for (;;)
+				{
+					CheckSDLEvents();
+					Interpret();
+				}
+			}
+		case MENUID_INTERPRETER_RESET:
+			{
+				Reset();
+			}
+		}
+	break;
+	default:
+		return DefWindowProc(hwnd, msg, wParam, lParam);
+	break;
+	}
+	return 0;
+}
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
+	LPSTR lpCmdLine, int nCmdShow)
+{
+	wc.cbSize		 = sizeof(WNDCLASSEX);
+	wc.style		 = 0;
+	wc.lpfnWndProc	 = WndProc;
+	wc.cbClsExtra	 = 0;
+	wc.cbWndExtra	 = 0;
+	wc.hInstance	 = hInstance;
+	wc.hIcon		 = LoadIcon(NULL, IDI_APPLICATION);
+	wc.hCursor		 = LoadCursor(NULL, IDC_ARROW);
+	wc.hbrBackground = (HBRUSH)(COLOR_WINDOW+1);
+	wc.lpszMenuName  = MAKEINTRESOURCE(IDR_MENU1);
+	wc.lpszClassName = "Window Class";
+	wc.hIconSm		 = LoadIcon(NULL, IDI_APPLICATION);
+
+	if(!RegisterClassEx(&wc))
+	{
+		MessageBox(NULL, "Window Registration Failed!", "Error!",
+			MB_ICONEXCLAMATION | MB_OK);
+		return 0;
+	}
+
+	hwnd = CreateWindowEx(
+		WS_EX_CLIENTEDGE,
+		"Window Class",
+		"Miracle Chip8",
+		WS_OVERLAPPEDWINDOW,
+		CW_USEDEFAULT, CW_USEDEFAULT, 240, 120,
+		NULL, NULL, hInstance, NULL);
+
+	if(!hwnd)
+	{
+		MessageBox(NULL, "Window Creation Failed!", "Error!",
+		MB_ICONEXCLAMATION | MB_OK);
+	}
+
+	ShowWindow(hwnd, nCmdShow);
+	UpdateWindow(hwnd);
+
+	while(GetMessage(&Msg, hwnd, 0, 0) > 0)
+	{
+		TranslateMessage(&Msg);
+		DispatchMessage(&Msg);
+    }
+	return Msg.wParam;
+}
---
+++ sdlfunc.c
@@ -0,0 +1,336 @@
+#include <sdl.h>
+#include <windows.h>
+
+extern unsigned char keyStatus[];
+
+extern UpdateTimers(Uint32 interval, void *param);
+
+extern HWND hwnd;
+
+SDL_Surface *screen;
+SDL_Window *sdlWindow;
+SDL_Renderer *renderer;
+SDL_Texture *texture;
+SDL_Color colors[2];
+
+const short int SCREEN_WIDTH = 640;
+const short int SCREEN_HEIGHT = 320;
+const short int SCREEN_BPP = 8;
+
+unsigned char color0;
+unsigned char color1;
+
+unsigned char screenData[64 * 32];
+
+unsigned char *screenPtr;
+
+SDL_Event event;
+SDL_TimerID timerID;
+
+int InitializeSDL()
+{
+	if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_TIMER) == -1)
+	{
+		atexit(SDL_Quit);
+		return -1;
+	}
+
+	return 0;
+}
+
+int OpenSDLWindow()
+{
+	sdlWindow = SDL_CreateWindowFrom(hwnd);
+	if (sdlWindow == NULL)
+		return 0;
+	renderer = SDL_CreateRenderer(sdlWindow, -1, SDL_RENDERER_SOFTWARE);
+	if (renderer == NULL)
+		return 0;
+	SDL_RenderSetLogicalSize(renderer, 64, 32);
+	
+	screen = SDL_CreateRGBSurfaceFrom(&screenData, 64, 32, 8, 64, 0, 0, 0, 0);
+
+	//screen = SDL_SetVideoMode(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_BPP, SDL_HWSURFACE | SDL_DOUBLEBUF);
+	//if (screen == NULL)
+	//	return -1;
+	
+	//screenPtr = (unsigned char *)screen->pixels;
+
+	colors[0].r = 0x00;
+	colors[0].g = 0x00;
+	colors[0].b = 0xFF;
+
+	colors[1].r = 0x00;
+	colors[1].g = 0xFF;
+	colors[1].b = 0xFF;
+
+	SDL_SetPaletteColors(screen->format->palette, colors, 0, 2);
+	SDL_SetWindowSize(sdlWindow, SCREEN_WIDTH, SCREEN_HEIGHT);
+
+	//	color0 = SDL_MapRGB(screen->format, 0x00, 0x00, 0xFF);
+	//	color1 = SDL_MapRGB(screen->format, 0x00, 0xFF, 0xFF);
+
+	return 0;
+}
+
+void CloseSDL()
+{
+	SDL_Quit();
+}
+
+/*int PlotPixel(int x, int y, int color)
+{
+	if (SDL_MUSTLOCK(screen) == -1)
+		if (SDL_LockSurface(screen) == -1)
+			return -1;
+
+	*(screenPtr + (y * screen->pitch + x)) = color;
+	
+	if (SDL_MUSTLOCK(screen) == -1)
+		SDL_UnlockSurface(screen);
+
+	return 0;
+}*/
+
+int UpdateDisplay()
+{
+/*	int i;
+	int j;
+
+	SDL_Rect plotRectangle;
+
+	for (j = 0; j < 32; j++)
+	{
+		for (i = 0; i < 64; i++)
+		{
+			plotRectangle.x = i * 9;
+			plotRectangle.y = j * 9;
+			plotRectangle.w = 9;
+			plotRectangle.h = 9;
+
+			switch(screenData[(j * 64) + i])
+			{
+			case 0:
+				{
+					SDL_FillRect(screen, &plotRectangle, color0);
+				}
+			break;
+			case 1:
+				{
+					SDL_FillRect(screen, &plotRectangle, color1);
+				}
+			break;
+			}
+		}
+	}*/
+	SDL_Delay(10);
+
+	SDL_memcpy(screen->pixels, screenData, 0x800);
+	texture = SDL_CreateTextureFromSurface(renderer, screen);
+	SDL_RenderClear(renderer);
+	SDL_RenderCopy(renderer, texture, 0, 0);
+	SDL_DestroyTexture(texture);
+	SDL_RenderPresent(renderer);
+	return 0;
+}
+
+int CheckSDLEvents()
+{
+	while(SDL_PollEvent(&event))
+	{
+		switch(event.type)
+		{
+		case SDL_KEYDOWN:
+			{
+				switch(event.key.keysym.sym)
+				{
+				case SDLK_7:
+					{
+						keyStatus[0x1] = 1;
+					}
+				break;
+				case SDLK_8:
+					{
+						keyStatus[0x2] = 1;
+					}
+				break;
+				case SDLK_9:
+					{
+						keyStatus[0x3] = 1;
+					}
+				break;
+				case SDLK_0:
+					{
+						keyStatus[0xC] = 1;
+					}
+				break;
+				case SDLK_u:
+					{
+						keyStatus[0x4] = 1;
+					}
+				break;
+				case SDLK_i:
+					{
+						keyStatus[0x5] = 1;
+					}
+				break;
+				case SDLK_o:
+					{
+						keyStatus[0x6] = 1;
+					}
+				break;
+				case SDLK_p:
+					{
+						keyStatus[0xD] = 1;
+					}
+				break;
+				case SDLK_j:
+					{
+						keyStatus[0x7] = 1;
+					}
+				break;
+				case SDLK_k:
+					{
+						keyStatus[0x8] = 1;
+					}
+				break;
+				case SDLK_l:
+					{
+						keyStatus[0x9] = 1;
+					}
+				break;
+				case SDLK_SEMICOLON:
+					{
+						keyStatus[0xE] = 1;
+					}
+				break;
+				case SDLK_m:
+					{
+						keyStatus[0xA] = 1;
+					}
+				break;
+				case SDLK_COMMA:
+					{
+						keyStatus[0x0] = 1;
+					}
+				break;
+				case SDLK_PERIOD:
+					{
+						keyStatus[0xB] = 1;
+					}
+				break;
+				case SDLK_SLASH:
+					{
+						keyStatus[0xF] = 1;
+					}
+				break;
+				}
+			}
+			break;
+		case SDL_KEYUP:
+			{
+				switch(event.key.keysym.sym)
+				{
+				case SDLK_7:
+					{
+						keyStatus[0x1] = 0;
+					}
+				break;
+				case SDLK_8:
+					{
+						keyStatus[0x2] = 0;
+					}
+				break;
+				case SDLK_9:
+					{
+						keyStatus[0x3] = 0;
+					}
+				break;
+				case SDLK_0:
+					{
+						keyStatus[0xC] = 0;
+					}
+				break;
+				case SDLK_u:
+					{
+						keyStatus[0x4] = 0;
+					}
+				break;
+				case SDLK_i:
+					{
+						keyStatus[0x5] = 0;
+					}
+				break;
+				case SDLK_o:
+					{
+						keyStatus[0x6] = 0;
+					}
+				break;
+				case SDLK_p:
+					{
+						keyStatus[0xD] = 0;
+					}
+				break;
+				case SDLK_j:
+					{
+						keyStatus[0x7] = 0;
+					}
+				break;
+				case SDLK_k:
+					{
+						keyStatus[0x8] = 0;
+					}
+				break;
+				case SDLK_l:
+					{
+						keyStatus[0x9] = 0;
+					}
+				break;
+				case SDLK_SEMICOLON:
+					{
+						keyStatus[0xE] = 0;
+					}
+				break;
+				case SDLK_m:
+					{
+						keyStatus[0xA] = 0;
+					}
+				break;
+				case SDLK_COMMA:
+					{
+						keyStatus[0x0] = 0;
+					}
+				break;
+				case SDLK_PERIOD:
+					{
+						keyStatus[0xB] = 0;
+					}
+				break;
+				case SDLK_SLASH:
+					{
+						keyStatus[0xF] = 0;
+					}
+				break;
+				}
+			}
+		break;
+		}
+	}
+	return 0;
+}
+
+void StartTimer()
+{
+	timerID = SDL_AddTimer((1000 / 60), UpdateTimers, NULL);
+}
+
+void RemoveTimer()
+{
+	if (timerID)
+		SDL_RemoveTimer(timerID);
+}
+
+void PlayBeep()
+{
+
+}
